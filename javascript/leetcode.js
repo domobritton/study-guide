3d602d661089dfce78190917f2670b29e2441d38
Domonic Adams <domonicbritton@gmail.com>
2018-12-13 11:34:53 -0800

    Update leetcode.js

--- javascript/leetcode.js
+++ javascript/leetcode.js
@@ -318,3 +318,15 @@ var removeDuplicates = function(nums) {
     }
     return i + 1;
 };
+
+var removeElement = function(nums, val) {
+    
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] !== val) {
+            nums[i] = nums[j]
+            i++;
+        }
+    }
+    return i;
+};
