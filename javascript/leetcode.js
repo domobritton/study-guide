8eec64a934222f0def71e823acc5a32056752852
Domonic Adams <domonicbritton@gmail.com>
2018-12-12 17:49:11 -0800

    Update leetcode.js

--- javascript/leetcode.js
+++ javascript/leetcode.js
@@ -275,3 +275,17 @@ var isPalindrome = function (x) {
 
 const a = isPalindrome(1001)
 console.log(a)
+
+// remove duplicates in place returning length
+
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) { return 0}
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+};
