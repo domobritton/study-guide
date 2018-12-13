f1c8396e4f74b71542b5e385e2f6814fd78df7a3
Domonic Adams <domonicbritton@gmail.com>
2018-12-13 12:39:07 -0800

    Update leetcode.js

--- javascript/leetcode.js
+++ javascript/leetcode.js
@@ -15,6 +15,26 @@ var strStr = function(haystack, needle) {
     return -1;
 };
 
+// Given a sorted array and a target value, return the index if the target is found. 
+// If not, return the index where it would be if it were inserted in order.
+// You may assume no duplicates in the array.
+var searchInsert = function(nums, target) {
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return i
+        }
+        if (i === nums.length - 1 && nums[i] < target) {
+            return i + 1;
+        }
+        if (nums[i] > target && nums[i - 1] < target) {
+            return i;
+        }
+    }
+    
+    return 0;
+};
+
 var threeSum = function(nums) {
     	var rtn = [];
 	if (nums.length < 3) {
