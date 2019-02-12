dce21d8c0a380cf640dd6f1348b843f0beae9b67
Domonic Adams <domonicbritton@gmail.com>
2019-02-11 17:45:56 -0800

    Update interviews.js

--- javascript/interviews.js
+++ javascript/interviews.js
@@ -26,3 +26,22 @@ function arithmeticBoggle(magicNumber, numbers) {
 
 console.log(arithmeticBoggle(2, [1, 2, 3, 4]));
 console.log(arithmeticBoggle(-42, [1, 13, 57, 17, 19, 35]));
+
+const repeated = (arr, k) => {
+  let counterHash = {};
+
+  arr.forEach(word => {
+      if (counterHash.hasOwnProperty(word)) {
+          counterHash[word] += 1;
+      } else {
+          counterHash[word] = 1;
+      }
+  });
+  return Object.keys(counterHash).find(key => counterHash[key] === k);
+}
+
+const answer = repeated(['red', 'red', 'blue', 'blue', 'blue', 'green'], 2);
+console.log(answer);
+
+// The forEach loop is O(n) linear time because it traverses every element of the array and stores it into an Object.
+// Object.keys is also O(n) because its look looking at each key in the object and filtering based on the .find criteria.
